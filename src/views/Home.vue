2f272b4b2b4fa6f69cd965e75209cf7b0af66cdc
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-06-22 15:33:20 +0600

    Rx base init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app>
+  <v-app-bar
+      elevation="0"
+      app
+      height="73px"
+      style="border-bottom:1px solid #e7e7e7 !important"
+      color="white"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 mr-2"
+          contain
+          min-width="100"
+          src="../assets/hero-logo.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        color="primary"
+        to="#"
+        text
+      >
+        <v-icon>mdi-prescription</v-icon>
+        <span class="mr-2">New Prescription</span>
+      </v-btn>
+    </v-app-bar>
+    <side-nav></side-nav>
+    <v-main>
+     <router-view/>
+    </v-main>
+</v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SideNav from '../views/SideNav.vue'
 export default {
-  name: 'Home',
+  name: 'App',
   components: {
-    HelloWorld
+    'side-nav': SideNav
+  },
+  data () {
+    return {
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/SideNav.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-navigation-drawer
+        dark
+        app
+        permanent
+        expand-on-hover
+      >
+        <v-list>
+          <v-list-item class="px-2" link>
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Dr. Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>MBBS, Dhaka Medical College</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-prescription</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Create Presciption</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-clipboard-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Create Appointment</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-clipboard-text</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Appointment List</v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
