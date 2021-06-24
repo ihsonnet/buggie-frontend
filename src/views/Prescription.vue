44ca43f2b5573cb1218e9e5ebd84dfffa7815ba7
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-06-24 18:00:53 +0600

    drug list added

--- src/views/Prescription.vue
+++ src/views/Prescription.vue
@@ -125,48 +125,116 @@
                                         </v-text-field>
                                     </v-col>
                                 </v-row>
-                                <v-row class="rowise">
-                                    <v-col>
-                                        <v-autocomplete
-                                            v-model="input"
-                                            :items="select"
-                                            placeholder="Search Drug Name"
-                                            class="mt-2 pa-0"
-                                            outlined
-                                            color="teal"
-                                            dense
-                                        >
-                                            <template slot="append">
-                                                <v-btn depressed class="mt-0" small style="vertical-align:center;margin-top:-2px !important; margin-right:-4px !important" @click="show">
-                                                    <v-icon>mdi-plus</v-icon>
-                                                </v-btn>
-                                            </template>
-                                        </v-autocomplete>
-                                    </v-col>
-                                    <v-col>
-                                        <v-autocomplete
-                                            v-model="input"
-                                            :items="select"
-                                            placeholder="Select a Dose"
-                                            class="mt-2 pa-0"
-                                            outlined
-                                            color="teal"
-                                            dense
-                                        >
-                                            <template slot="append">
-                                                <v-btn  depressed class="mt-0" small style="vertical-align:center;margin-top:-2px !important; margin-right:-4px !important" @click="show">
-                                                    <v-icon>mdi-plus</v-icon>
-                                                </v-btn>
-                                            </template>
-                                        </v-autocomplete>
-                                    </v-col>
-                                </v-row>
                             </v-card>
                         </v-col>
+                   </v-row>
+                   <v-row>
+                       <v-col>
+                           <v-card class="pa-4 mt-2" elevation="0" style="border: 1px solid #e7e7e7" width="100%">
+                               <v-row class="pa-5">
+                                   <v-icon large>mdi-prescription</v-icon>  <v-spacer></v-spacer> <v-btn depressed @click="adddialog = true" color="info">Add Drugs</v-btn>
+                               </v-row>
+                               <v-row style="background-color:#f2f5f8;border-radius:8px;text-align:center">
+                                   <v-col cols="3">
+                                       <b>Brand</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Dose</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Instruction</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Duration</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Note</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Action</b>
+                                   </v-col>
+                               </v-row>
+                               <v-row v-for="drug in drugList" :key="drug" style="text-align:center;border-bottom: 1px solid #e7e7e7">
+                                    <v-col cols="3">
+                                        <v-card-subtitle>
+                                            {{ drug.brand }}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            {{ drug.dose }}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            {{ drug.instruction }}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            {{ drug.duration }}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            {{ drug.note }}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            <v-btn-group>
+                                                <v-btn color="info" depressed small><v-icon small>mdi-pencil-outline</v-icon></v-btn><v-btn color="error" depressed small><v-icon small>mdi-delete</v-icon></v-btn>
+                                            </v-btn-group>
+                                        </v-card-subtitle>
+                                    </v-col>
+                               </v-row>
+                           </v-card>
+                       </v-col>
                    </v-row>
                 </v-card>
             </v-col>
     </v-row>
+
+
+
+    // dialog here
+
+    <v-dialog title="Add New Drug" v-model="adddialog" max-width="800px">
+        <v-card class="pa-5">
+            <h3>Add New Drug</h3>
+            <v-row class="rowise pt-5">
+            <v-col cols="6" v-for="i in 4" :key="i">
+                <v-autocomplete
+                    v-model="input"
+                    :items="select"
+                    placeholder="Search Drug Name"
+                    class="mt-2 pa-0"
+                    outlined
+                    color="teal"
+                    dense
+                >
+                    <template slot="append">
+                        <v-btn depressed class="mt-0" small style="vertical-align:center;margin-top:-2px !important; margin-right:-4px !important" @click="show">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                    </template>
+                </v-autocomplete>
+            </v-col>
+        </v-row>
+        <v-textarea
+        style="margin-top:0px !important"
+        name="input-7-1"
+        outlined
+        background-color="white"
+        color="teal"
+        auto-grow
+        placeholder="Write Note..."
+        ></v-textarea>
+        <v-btn-group>
+            <v-btn depressed color="info"><v-icon class="mr-2" @click="adddialog = false">mdi-content-save</v-icon> Save Drug</v-btn>
+        </v-btn-group>
+        </v-card>
+    </v-dialog>
     </div>
 </template>
 <script>
@@ -174,6 +242,7 @@ export default {
     data() {
         return {
             input: "",
+            adddialog: false,
             sideItems: [
                 {
                     testTitle: "Cheif Complaints",
@@ -200,7 +269,23 @@ export default {
                 href: 'rx-prescription',
                 },
             ],
-            select: ["Napa","Bexted","Koroz"]
+            select: ["Napa","Bexted","Koroz"],
+            drugList: [
+                {
+                    brand: "Ace | Paracetamol | 500mg",
+                    dose: "১+০+১",
+                    instruction: "খাবার পরে খাবেন",
+                    duration: "৭ দিন",
+                    note: "",
+                },
+                {
+                    brand: "Napa Extra | Paracetamol | 200mg",
+                    dose: "১+০+১",
+                    instruction: "খাবার পরে খাবেন",
+                    duration: "১৫ দিন",
+                    note: " -",
+                },
+            ]
         }
     },
     methods: {
@@ -211,7 +296,8 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.rowise .col {
+.rowise .col-6{
     padding-bottom: 0px !important;
+    padding-top: 0px !important;
 }
 </style>
